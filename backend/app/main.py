c84f66f9ba3417e991e2ef4718fea2d31346c8c9
iknowsummer <iknowsummer@gmail.com>
2025-09-13 15:48:05 +0900

    [API] 書籍ランダム取得のAPIを実装

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Query
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func
 from database import Base, engine, SessionLocal
 from models import Book
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 app = FastAPI()
 
 
@@ -22,6 +27,23 @@ def get_db():
 @app.get("/")
 def root():
     return {"message": "Hello, FastAPI!"}
+
+
+# ランダムで指定数の書籍を取得するエンドポイント
+@app.get("/books/pick/")
+def pick_books(
+    db: Session = Depends(get_db),
+    include_picked: int = Query(0, description="1でis_picked=1も含める。"),
+):
+    pickcount = int(os.getenv("PICKCOUNT", 4))
+
+    if include_picked == 0:
+        query = db.query(Book).filter(Book.is_picked != 1)
+    else:
+        query = db.query(Book)
+
+    books = query.order_by(func.random()).limit(pickcount).all()
+    return books
 
 
 @app.post("/books/")
