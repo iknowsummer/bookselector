822a5d80e700df6e7945578180ca8a6e09f26ef2
iknowsummer <iknowsummer@gmail.com>
2025-09-22 07:01:24 +0900

    [Next] booksページを仮作成

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingRoot: __dirname,
+  images: {
+    domains: ["books.google.com"],
+  },
 };
 
 export default nextConfig;
