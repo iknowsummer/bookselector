421206bcba21ed82793a04f55b75a7df569498cd
iknowsummer <89126670+iknowsummer@users.noreply.github.com>
2025-09-25 09:16:25 +0900

    [API] ルーティングをファイル分割

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,3 +9,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    """リクエストごとにSQLAlchemyセッションを管理する依存関係"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
