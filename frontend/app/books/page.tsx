d6796ec0007eb3c8d330188dc8066d016ac24362
iknowsummer <iknowsummer@gmail.com>
2025-10-12 23:55:51 +0900

    [Next] /booksページのpage.tsxをコンポーネント利用に改修

--- frontend/app/books/page.tsx
+++ frontend/app/books/page.tsx
@@ -1,63 +1,36 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import type { Book } from "@/types/book";
 import { fetchBooks } from "@/lib/api/books";
+import { BookList } from "@/components/BookList";
 
 export default function Home() {
   const [books, setBooks] = useState<Book[]>([]);
   const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getData = async () => {
+      setIsLoading(true);
+      setError("");
       try {
         const data = await fetchBooks();
         setBooks(data);
       } catch (err) {
         setError(`API取得エラー: ${err}`);
+      } finally {
+        setIsLoading(false);
       }
     };
     getData();
   }, []);
 
   return (
-    <>
-      {error ? (
-        <div>エラー: {error}</div>
-      ) : (
-        <div>
-          <div className="header-with-button">
-            <h2>書籍一覧</h2>
-            <Link href="/books/new">
-              <button>新規登録</button>
-            </Link>
-          </div>
-          <ul className="book-list">
-            {books.map((book) => (
-              <li key={book.id}>
-                <Link href={`/books/${book.id}`}>
-                  <div className="book-image">
-                    {book.image_url ? (
-                      <Image
-                        src={book.image_url}
-                        alt={book.title}
-                        width={128}
-                        height={200}
-                      />
-                    ) : (
-                      <span className="noimage">No Image</span>
-                    )}
-                  </div>
-                  <div className="book-title">{book.title ?? ""}</div>
-                  <div className="book-author">{book.author ?? ""}</div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </>
+    <div>
+      <h2>書籍一覧</h2>
+      <BookList books={books} error={error} isLoading={isLoading} />
+    </div>
   );
 }
