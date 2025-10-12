dd34972f219879e006f0beba8679b42300eb25cc
iknowsummer <iknowsummer@gmail.com>
2025-10-12 23:30:16 +0900

    [Next] BookListを改修し、リンク付きのカード形式へ変更

--- frontend/components/BookList.tsx
+++ frontend/components/BookList.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 import type { Book } from "@/types/book";
 
 type BookListProps = {
@@ -6,13 +8,25 @@ type BookListProps = {
 
 export function BookList({ books }: BookListProps) {
   return (
-    <ul>
+    <ul className="book-list">
       {books.map((book) => (
         <li key={book.id}>
-          <div>タイトル: {book.title}</div>
-          <div>著者: {book.author}</div>
-          {book.description ? <div>説明: {book.description}</div> : null}
-          {book.note ? <div>メモ: {book.note}</div> : null}
+          <Link href={`/books/${book.id}`}>
+            <div className="book-image">
+              {book.image_url ? (
+                <Image
+                  src={book.image_url}
+                  alt={book.title}
+                  width={128}
+                  height={200}
+                />
+              ) : (
+                <span className="noimage">No Image</span>
+              )}
+            </div>
+            <div className="book-title">{book.title ?? ""}</div>
+            <div className="book-author">{book.author ?? ""}</div>
+          </Link>
         </li>
       ))}
     </ul>
