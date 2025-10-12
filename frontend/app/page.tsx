695b42c24ba3f2c789bcab0fe9cf87b159ae5f78
iknowsummer <iknowsummer@gmail.com>
2025-10-12 23:13:18 +0900

    [Next] ランダム取得機能をコンポーネントとして分離

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,52 +1,12 @@
-"use client";
-import { useState, useEffect } from "react";
-import type { Book } from "@/types/book";
-import { fetchRandomBooks } from "@/lib/api/books";
-import { BookList } from "@/components/BookList";
+import RandomBookList from "@/components/RandomBookList";
 
 export default function Home() {
-  const [books, setBooks] = useState<Book[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>("");
-
-  const handleFetchRandomBooks = async () => {
-    setIsLoading(true);
-    setError("");
-    try {
-      const data = await fetchRandomBooks();
-      setBooks(data);
-      if (data.length === 0) {
-        setError("書籍情報が見つかりませんでした");
-      }
-    } catch (err) {
-      setBooks([]);
-      setError(`書籍情報の取得に失敗しました: ${err}`);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    handleFetchRandomBooks();
-  }, []);
-
   return (
     <main>
       <section>
         <h1>ランダム取得</h1>
       </section>
-      <section>
-        <button type="button" onClick={handleFetchRandomBooks}>
-          ランダム再読込
-        </button>
-        {isLoading ? (
-          <div>書籍情報を取得中です...</div>
-        ) : error ? (
-          <div>{error}</div>
-        ) : (
-          <BookList books={books} />
-        )}
-      </section>
+      <RandomBookList />
     </main>
   );
 }
