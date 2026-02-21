d72d5b1676f2962b2ed16a4bffb1408f16e47da6
iknowsummer <iknowsummer@gmail.com>
2026-02-21 23:45:20 +0900

    [Docker] compose, Dockerfile作成

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
   outputFileTracingRoot: __dirname,
   images: {
     domains: ["books.google.com"],
